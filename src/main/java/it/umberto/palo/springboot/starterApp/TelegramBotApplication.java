385232ca9bf923986f01ee6855db6a9856f4e47f
Umberto Palo <u.palo@U-PALO.replynet.prv>
2017-10-25 15:07:01 +0200

    agginta dipendenza librerire di telegram e ultima versione di spring bot 1.5.8

--- src/main/java/it/umberto/palo/springboot/starterApp/TelegramBotApplication.java
+++ src/main/java/it/umberto/palo/springboot/starterApp/TelegramBotApplication.java
@@ -2,11 +2,13 @@ package it.umberto.palo.springboot.starterApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * This is a standard spring-boot main class.
  */
 @SpringBootApplication
+@ComponentScan(basePackages= {"it.umberto.palo.camel.route","it.umberto.palo.bot"})
 public class TelegramBotApplication {
 
     public static void main(String[] args) throws Exception {
