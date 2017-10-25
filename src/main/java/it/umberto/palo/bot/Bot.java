6538aa7f0b021a99011259f73ad78ac4cf5f6c08
Umberto Palo <u.palo@U-PALO.replynet.prv>
2017-10-25 14:35:30 +0200

    springbootcamel primo commit

---
+++ src/main/java/it/umberto/palo/bot/Bot.java
@@ -0,0 +1,31 @@
+package it.umberto.palo.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class contains the chat-bot logic: use your fantasy to implement your own Bot.
+ */
+@Component
+public class Bot {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * This method processes incoming messages and return responses.
+     *
+     * @param message a message coming from a human user in a chat
+     * @return the reply of the bot. Return null if you don't want to answer
+     */
+    public String process(String message) {
+        if (message == null) {
+            return null; // skip non-text messages
+        }
+
+        log.info("Received message: {}", message);
+
+        return "Why did you say \"" + message.replace("\"", "-") + "\"?";
+    }
+
+}
---
+++ src/main/java/it/umberto/palo/camel/route/CamelRouter.java
@@ -0,0 +1,26 @@
+package it.umberto.palo.camel.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import it.umberto.palo.bot.Bot;
+
+/**
+ * This class is responsible for routing the messages from and to the Telegram chat.
+ */
+@Component
+public class CamelRouter extends RouteBuilder {
+
+    @Autowired
+    private Bot bot;
+
+    @Override
+    public void configure() throws Exception {
+
+        from("telegram:bots")
+        .bean(bot)
+        .to("telegram:bots");
+
+    }
+}
